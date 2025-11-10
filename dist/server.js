6a733dbec3caa5d144e589831a0b72f93ca527fb
samakhraim <samakhraim12@gmail.com>
2025-11-10 19:31:01 +0200

    updated

--- dist/server.js
+++ dist/server.js
@@ -6,12 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const users_1 = __importDefault(require("./handlers/users"));
+const products_1 = __importDefault(require("./handlers/products"));
+const orders_1 = __importDefault(require("./handlers/orders"));
+const orderProducts_1 = __importDefault(require("./handlers/orderProducts"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.get('/', (req, res) => {
-    res.send(' Storefront backend is running!');
+    res.send('Storefront backend is running!');
 });
 (0, users_1.default)(app);
+(0, products_1.default)(app);
+(0, orders_1.default)(app);
+(0, orderProducts_1.default)(app);
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
--- dist/tests/productSpec.js
+++ dist/tests/productSpec.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const product_1 = require("../models/product");
 const store = new product_1.ProductStore();
 describe('Product Model', () => {
+    let createdProductId;
     it('should have an index method', () => {
         expect(store.index).toBeDefined();
     });
     it('create method should add a product', async () => {
         const result = await store.create({ name: 'Book', price: 15 });
+        createdProductId = result.id.toString();
         expect(result.name).toBe('Book');
     });
     it('index method should return a list of products', async () => {
@@ -15,12 +17,12 @@ describe('Product Model', () => {
         expect(result.length).toBeGreaterThan(0);
     });
     it('update method should modify a product', async () => {
-        const result = await store.update('1', { name: 'Updated Book', price: 20 });
+        const result = await store.update(createdProductId, { name: 'Updated Book', price: 20 });
         expect(result.name).toBe('Updated Book');
     });
     it('delete method should remove the product', async () => {
-        const result = await store.delete('1');
-        expect(result.id).toBe(1);
+        const result = await store.delete(createdProductId);
+        expect(result.id).toBe(Number(createdProductId));
     });
 });
 //# sourceMappingURL=productSpec.js.map
