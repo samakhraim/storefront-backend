5a07a800e41ad0add0a5ca37f5af48c62ae1e813
samakhraim <samakhraim12@gmail.com>
2025-11-18 12:47:39 +0200

    updated nav bar

--- mystore-frontend/src/app/components/navbar/navbar.component.ts
+++ mystore-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 
 @Component({
-  
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive],
@@ -13,13 +12,38 @@ import { CartService } from '../../services/cart.service';
 })
 export class NavbarComponent implements OnInit {
   cartCount = 0;
-isMenuOpen = false;
+  isMenuOpen = false;
+  isLoggedIn = false;
+  userName: string | null = null;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private router: Router) {}
 
   ngOnInit(): void {
+    // Update cart count
     this.cartService.cartItems$.subscribe(items => {
       this.cartCount = items.length;
     });
+
+    // Check login status
+    this.isLoggedIn = !!localStorage.getItem('token');
+
+    // Load user's name
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const userObj = JSON.parse(storedUser);
+      this.userName = userObj.first_name;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.isLoggedIn = false;
+    this.userName = null;
+
+    this.isMenuOpen = false;
+
+    // Redirect to the product page
+    this.router.navigate(['/']);
   }
 }
